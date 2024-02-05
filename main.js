849dfd0cea4a13e45912bac5ab32ed3a0416ed9a
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-02-05 18:22:16 +0500

    learning

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const productsContainer = document.querySelector(".products-container");
 const loadMoreBtn = document.querySelector(".load-more-btn");
 let currentStep = 0;
 
-async function fetchListOfProducts() {
+async function fetchListOfProducts(getCurrentStep) {
 
     try {
 
-        const response = await fetch("https://dummyjson.com/products", {
-
-            method: "GET"
-        })
+        const response = await fetch(
+            `https://dummyjson.com/products?limit=10&skip=${getCurrentStep === 0 ? 0 : getCurrentStep * 10
+            }`,
+            {
+                method: "GET",
+            });
 
         const result = await response.json();
         console.log(result);
@@ -52,13 +54,20 @@ function displayProducts(productList) {
         productsContainer.appendChild(productItemWrapper);
     });
 
+    if (productsContainer.children.length === 100) {
+
+        loadMoreBtn.setAttribute("disabled", "true");
+    }
+
+    console.log(productsContainer.children.length);
 
 }
 
 
-fetchListOfProducts();
+fetchListOfProducts(currentStep);
 
 loadMoreBtn.addEventListener("click", () => {
 
+    fetchListOfProducts(currentStep += 1);
 
-})
+});
